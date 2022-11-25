24bd54b0200be92c69e5eafe8e86c92281e0aef1
zaac2240632 <zaac2240632@yahoo.com.tw>
2022-11-25 13:33:27 +0800

    create program and index

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+const app = express()
+const users = require('./models/users')
+const PORT = process.env.PORT || 3000
+
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
+
+require('./config/mongoose')
+
+app.get('/', (req, res) => {
+    res.render('index')
+})
+
+app.listen(PORT, () => {
+    console.log(`App is running on localhost:${PORT}`)
+})
