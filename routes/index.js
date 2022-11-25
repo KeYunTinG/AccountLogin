31963f39c9c5be78251b9760d274a8bc9daf6eed
KeYunTinG <zaac2240632@yahoo.com.tw>
2022-11-26 01:17:32 +0800

    check email and password

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const home = require('./modules/home.js')
+const home = require('./modules/home')
 
 router.use('/', home)
 
--- routes/modules/home.js
+++ routes/modules/home.js
@@ -6,5 +6,19 @@ const PORT = process.env.PORT || 3000
 router.get('/', (req, res) => {
   res.render('index')
 })
+router.post('/', (req, res) => {
+  const email = req.body.email
+  const password = req.body.password
+  users.findOne({ email })
+    .then((item) => {
+      if ((item) && (item.password === password)) {
+        res.send(`<h1>Welcome! ${item.firstName}</h1 >`)
+      } else {
+        res.send("Please Check Your Email or Password.")
+      }
+    })
+
+})
+
 
 module.exports = router
