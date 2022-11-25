e9d3359463c64072172b129c180873639c7f769c
zaac2240632 <zaac2240632@yahoo.com.tw>
2022-11-25 15:37:05 +0800

    feat: seeder

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
 const app = express()
-const users = require('./models/users')
+const users = require('./models/user')
 const PORT = process.env.PORT || 3000
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
